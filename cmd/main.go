8c550e3e218cb9367fa174ca91901a6090e0d9a7
Alex Acebo <aacebowork@gmail.com>
2024-07-10 08:44:38 -0700

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -15,9 +15,6 @@ func main() {
 	var adoClient *ado.Client
 	adoUrl := os.Getenv("AZURE_DEVOPS_URL")
 	adoToken := os.Getenv("AZURE_DEVOPS_TOKEN")
-	cmd := cli.NewCommand("ag").
-		WithDescription("MS CLI (Unoffcial)").
-		WithCommand(version.New())
 
 	if adoUrl != "" {
 		v, err := ado.New(adoUrl, adoToken)
@@ -29,10 +26,13 @@ func main() {
 		adoClient = v
 	}
 
-	cmd = cmd.WithCommand(
-		_ado.New(adoClient).
-			WithDisabled(adoClient == nil),
-	)
+	cmd := cli.NewCommand("ag").
+		WithDescription("MS CLI (Unoffcial)").
+		WithCommand(version.New()).
+		WithCommand(
+			_ado.New(adoClient).
+				WithDisabled(adoClient == nil),
+		)
 
 	if err := cmd.Run(os.Args[1:]...); err != nil {
 		fmt.Println(err)
